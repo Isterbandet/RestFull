3f1509767afd2671d629f632dfc9eef3853708cb
Anton Eklöf <anton_eklof@hotmail.com>
2018-03-15 17:39:33 +0100

    Slutet på dagen

--- src/java/RestFull/KompisarServiceX.java
+++ src/java/RestFull/KompisarServiceX.java
@@ -2,6 +2,7 @@ package RestFull;
 
 import java.util.List;
 import javax.ws.rs.GET; 
+import javax.ws.rs.POST;
 import javax.ws.rs.Path; 
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces; 
@@ -17,7 +18,7 @@ public class KompisarServiceX {
     @Path("/kompisarJSON")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Kompisar> getKompisarJSON(){
-        return kompisdao.getAllKompisar();
+        return allaKompisar;//kompisdao.getAllKompisar();
     }
     @GET
     @Path("/kompisarJSON/{namn}")
@@ -38,23 +39,61 @@ public class KompisarServiceX {
     @Produces(MediaType.APPLICATION_JSON)
     public Response deleteNameByName(@PathParam("namn") String namn){
         Response res = new Response("Person borttagen", Boolean.FALSE);
+        Response inte = new Response("Personen finns inte ", Boolean.FALSE);
         
-        int indexToRemove= -1;
-        for (int i=0; i < allaKompisar.size(); i++){
-            if (allaKompisar.get(i).getNamn().equals(namn)){
-                indexToRemove = i;
+        boolean inList = false;
+        int idToRemove = -1;
+        Kompisar kompisToRemove = null;
+        
+        for(Kompisar k : allaKompisar ){
+            if (k.getNamn().equals(namn)){
+                kompisToRemove = k;
             }
         }
-        if (indexToRemove != 1){
-            allaKompisar.remove(indexToRemove);
-            res.setBoolean(Boolean.TRUE);
-  
+        
+        if (kompisToRemove != null){
+            allaKompisar.remove(kompisToRemove);
+            return res;
         }
+        return inte;
+    }
+    @POST
+    @Path("kompisar/add")
+    //@Produces(MediaType.APPLICATION_JSON)
+    public Response addKompis(Kompisar k){
+        Response res = new Response("Kompis addad", Boolean.FALSE);
+        allaKompisar.add(k);
+        res.setBoolean(Boolean.TRUE);
         return res;
+        
+    }
+    
+
+   
+}
+        
+//        int indexToRemove= -1;
+//        for (int i=0; i < allaKompisar.size(); i++){
+//            if (allaKompisar.get(i).getNamn().equals(namn)){
+//                indexToRemove = i;
+//           }
+//        }
+//        
+//        if (!allaKompisar.contains(namn)){
+//            allaKompisar.remove(indexToRemove);
+//            return inte;//res.getMessage("Personen HIttades inte!", Boolean.FALSE);
+//        }
+//        
+//        
+//        if (indexToRemove != 1){
+//            allaKompisar.remove(indexToRemove);
+//            res.setBoolean(Boolean.TRUE);
+//  
+//        }
+        //return res;
     
         
         
-    }
-}
+ 
 
 //http://www.localhost:8080/RestFull/rest/KompisarServiceX/kompisarJSON
