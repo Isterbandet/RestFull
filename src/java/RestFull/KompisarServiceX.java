b667e6a16643047edc1093c5a5eac31cc5c73e12
Anton Eklöf <anton_eklof@hotmail.com>
2018-03-15 10:07:37 +0100

    Delete Funktionalitet

--- src/java/RestFull/KompisarServiceX.java
+++ src/java/RestFull/KompisarServiceX.java
@@ -9,8 +9,8 @@ import javax.ws.rs.core.MediaType;
 @Path("/KompisarServiceX")
 
 public class KompisarServiceX {
-    KompisarDao kompisdao = new KompisarDao();
-    List<Kompisar> allaKompisar = kompisdao.getAllKompisar();
+    private static KompisarDao kompisdao = new KompisarDao();
+    private static List<Kompisar> allaKompisar = kompisdao.getAllKompisar();
     
     
     @GET
@@ -24,8 +24,37 @@ public class KompisarServiceX {
     @Produces(MediaType.APPLICATION_JSON)
     public Kompisar hittaPolare(@PathParam("namn") String namn){
         Kompisar komp = new Kompisar();
-        for (Kompisar k: kompisar )
-    
+        for (Kompisar k: allaKompisar ){
+            if (k.getNamn().equals(namn)){
+                komp = k;
+            }
+        
     }
-
+    return komp;
+    }
+    
+    @GET
+    @Path("/kompisarJSON/{namn}/delete")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteNameByName(@PathParam("namn") String namn){
+        Response res = new Response("Person borttagen", Boolean.FALSE);
+        
+        int indexToRemove= -1;
+        for (int i=0; i < allaKompisar.size(); i++){
+            if (allaKompisar.get(i).getNamn().equals(namn)){
+                indexToRemove = i;
+            }
+        }
+        if (indexToRemove != 1){
+            allaKompisar.remove(indexToRemove);
+            res.setBoolean(Boolean.TRUE);
+  
+        }
+        return res;
+    
+        
+        
+    }
 }
+
+//http://www.localhost:8080/RestFull/rest/KompisarServiceX/kompisarJSON
